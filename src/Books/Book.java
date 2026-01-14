98b85b8c47e90c63af43b54a34c1b06ff6204c5e
Leonardo2831 <leoferraz1818@gmail.com>
2026-01-13 23:24:43 -0300

    new structure book

--- src/Books/Book.java
+++ src/Books/Book.java
@@ -1,10 +1,36 @@
 package Books;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Book {
-    private List<Book> books = new ArrayList<>();
-    private List<Author> autores = new ArrayList<>();
-    private List<Loan> loans = new ArrayList<>();
+    int id;
+    String title;
+    String author;
+    LocalDate publishDate;
+
+    boolean loan;
+    String userLoan;
+    LocalDateTime dateLoan;
+
+    static String formatDateTime(LocalDateTime dateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return dateTime.format(formatter);
+    }
+
+    static String formatDate(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(formatter);
+    }
+
+    public Book(int id, String title, String author, LocalDate publishDate, boolean loan, String userLoan, LocalDateTime dateLoan) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publishDate = publishDate;
+        this.loan = loan;
+        this.userLoan = userLoan;
+        this.dateLoan = dateLoan;
+    }
 }
